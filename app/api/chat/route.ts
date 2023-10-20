d1acd12106c4a6f5eb57bccf328cd2396ba213fb
Eric Er <86953900+ericermerimen@users.noreply.github.com>
2023-10-20 15:26:45 +0800

    add images, and adjust styling

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -24,6 +24,7 @@ export async function POST(req: Request) {
     model: "gpt-3.5-turbo",
     stream: true,
     messages: [prefix, ...messages],
+    // max_tokens: 60,
   });
 
   // Convert the response into a friendly text-stream
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useChat } from "ai/react";
+import loading0 from "./images/loading-0.gif";
+import loading1 from "./images/loading-1.gif";
+import loading2 from "./images/loading-2.gif";
+import loading3 from "./images/loading-3.gif";
+import loading4 from "./images/loading-4.gif";
+import mainpic from "./images/mainpic.jpg";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
+  // console.log("messages", messages);
   function mutateAssistantResponse(inputString: string) {
     // console.log(inputString);
     const splitString = inputString?.split("|");
@@ -21,8 +28,55 @@ export default function Chat() {
     const memeImage = resultArray[3]
       ? `https://apimeme.com/meme?meme=${resultArray[0]}&top=${resultArray[1]}&bottom=${resultArray[2]}`
       : null;
+
+    function loadingImage() {
+      switch ((messages?.length / 2) % 5) {
+        case 0:
+          return (
+            <>
+              <img src={loading0.src} alt="loading" />
+              loading...
+            </>
+          );
+          break;
+        case 1:
+          return (
+            <>
+              <img src={loading1.src} alt="loading" />
+              loading...
+            </>
+          );
+          break;
+        case 2:
+          return (
+            <>
+              <img src={loading2.src} alt="loading" />
+              loading...
+            </>
+          );
+          break;
+        case 3:
+          return (
+            <>
+              <img src={loading3.src} alt="loading" />
+              loading...
+            </>
+          );
+          break;
+        case 4:
+          return (
+            <>
+              <img src={loading4.src} alt="loading" />
+              loading...
+            </>
+          );
+          break;
+      }
+    }
+
     if (!resultArray[3] || resultArray[3].length <= 3)
-      return <>(Thinking...)</>;
+      return <>{loadingImage()}</>;
+    // return <>(Thinking...)</>;
     return (
       <>
         {memeImage !== null && <img src={memeImage} alt="meme" />}
@@ -32,10 +86,16 @@ export default function Chat() {
   }
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <img src={mainpic.src} alt="meme talk" />
       {messages.length > 0
         ? messages.map((m) => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === "user" ? "User: " : "AI: "}
+            <div key={m.id} className="whitespace-pre-wrap my-3">
+              <span
+                className="font-bold"
+                style={{ color: m.role === "user" ? "blue" : "red" }}
+              >
+                {m.role === "user" ? "You: " : "Not You: "}
+              </span>
               {m.role === "assistant"
                 ? mutateAssistantResponse(m.content)
                 : m.content}
@@ -47,8 +107,9 @@ export default function Chat() {
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="Wussup bro?"
           onChange={handleInputChange}
+          // minLength={12}
         />
       </form>
     </div>
