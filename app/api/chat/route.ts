fde8fd8f683c324fd8acda4ad27e87735b133fce
Eric Er <86953900+ericermerimen@users.noreply.github.com>
2023-10-20 14:23:05 +0800

    forgot to add prefix

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
-    messages: messages,
+    messages: [prefix, ...messages],
   });
 
   // Convert the response into a friendly text-stream
