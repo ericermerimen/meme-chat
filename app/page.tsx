a119c231fc875cc18046f6152195b3504722adba
Eric Er <86953900+ericermerimen@users.noreply.github.com>
2023-10-20 14:15:07 +0800

    mutate the response from ai, and use apimeme to get image

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,44 @@
-'use client';
+"use client";
 
-import { useChat } from 'ai/react';
+import { useChat } from "ai/react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
+  function mutateAssistantResponse(inputString: string) {
+    // console.log(inputString);
+    const splitString = inputString?.split("|");
+
+    // Remove the numbering and trim whitespace for each element
+    const resultArray = splitString.map((item) =>
+      item
+        .replace(/^\s*\d+\.\s*/, "")
+        .replace(/["'“”‘’]/g, "")
+        .trim()
+    );
+
+    // console.log(resultArray);
+    // https://apimeme.com/meme?meme=Clown-Applying-Makeup&top=wtf+bro&bottom='haha+you+lame'
+    const memeImage = resultArray[3]
+      ? `https://apimeme.com/meme?meme=${resultArray[0]}&top=${resultArray[1]}&bottom=${resultArray[2]}`
+      : null;
+    if (!resultArray[3] || resultArray[3].length <= 3)
+      return <>(Thinking...)</>;
+    return (
+      <>
+        <img src={memeImage} alt="meme" />
+        <p>{resultArray[3]}</p>
+      </>
+    );
+  }
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.length > 0
-        ? messages.map(m => (
+        ? messages.map((m) => (
             <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
+              {m.role === "user" ? "User: " : "AI: "}
+              {m.role === "assistant"
+                ? mutateAssistantResponse(m.content)
+                : m.content}
             </div>
           ))
         : null}
