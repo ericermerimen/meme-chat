5c13aeed273fc3a7f6a9caf8e301805a4b372f66
Eric Er <86953900+ericermerimen@users.noreply.github.com>
2023-10-23 10:31:21 +0800

    Amend title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Meme talk",
+  title: "Meme Chat",
   description: "bla bla blaaaa",
 };
 
